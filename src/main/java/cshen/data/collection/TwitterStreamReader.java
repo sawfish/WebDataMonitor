0ca3729a9d24242dab9fdae1a7da56fc541494e9
sawfish <charley.shen@gmail.com>
2013-08-22 20:13:39 -0400

    resources

--- src/main/java/cshen/data/collection/TwitterStreamReader.java
+++ src/main/java/cshen/data/collection/TwitterStreamReader.java
@@ -4,125 +4,99 @@ package cshen.data.collection;
  */
 
 
+import java.io.*;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.log4j.Logger;
+
 import twitter4j.FilterQuery;
-import twitter4j.StallWarning;
-import twitter4j.Status;
-import twitter4j.StatusDeletionNotice;
-import twitter4j.StatusListener;
+import twitter4j.RawStreamListener;
 import twitter4j.TwitterStream;
 import twitter4j.TwitterStreamFactory;
-import twitter4j.json.DataObjectFactory;
 
-/**
- * This is an abstract class to read twitter stream for event summarization app. 
- * A built-in {@link #buffer} holds crawled but unread tweets. 
- * Function {@link #read()} called by event summarization app.
- * @author shc2pal
- * 
- */
 public class TwitterStreamReader {
+	private static Logger log = Logger.getLogger(TwitterStreamReader.class);
 	public static void main(String[] args) throws Exception {
-		TwitterStreamReader streamReader = new TwitterStreamReader();
-		streamReader.setQuery(new String[] {"lmao"});
-		
-		if (streamReader.start()) {
-			while(true) {
-				List<String> tweets = streamReader.read();
-				for(int i = 0; i < tweets.size() && i < 2; i++) {
-					System.err.println(tweets.get(i)); 
-				}
-				if (tweets.size() > 2) 
-					System.err.println("And other " + (tweets.size() - 2) + "tweets\n");
-				
-				Thread.sleep(5000);
-			}
+		if (args.length < 1) {
+			usage();
+			System.exit(1);
 		}
+		String tweetsFilesDir = args[0];
+		String localQueryFile = args.length > 1 ? args[1] : null;
+		
+		run(tweetsFilesDir, localQueryFile);
 	}
-	protected boolean stop;
-	/**
-	 * buffer to hold unread tweets
-	 */
-	protected Queue<String> buffer = new LinkedList<String>();
-	
-	Thread loadThread = null;
-	
-	FilterQuery query;
-	TwitterStream twitterStream;
-	
-	// put preprocessor here is because we do not need to wait long time for preprocessing in debug
-	// finally it can be put out, or leave it here.
-	public TwitterStreamReader() throws Exception {
+
+	private static void usage() {
+		System.err.println("usage TwitterStreamReader <hdfs_tweets_list_dir> [local_query_file]");
+		
 	}
-	
-	public boolean end() {
-		twitterStream.shutdown();
-		stop = true;
-		return true;
-	}
-	
-	public boolean isFinished() {
-		return !loadThread.isAlive();
-	}
-	protected void load() {
-		StatusListener listener = new StatusListener() {
+
+	private static void run(final String tweetsFilesDir, String localQueryFile) throws IOException, InterruptedException {
+		Configuration conf = new Configuration();
+		final FileSystem hdfs = FileSystem.get(conf);
+		FileSystem local = FileSystem.getLocal(conf);
+		FilterQuery query = null;
+		
+		hdfs.mkdirs(new Path(tweetsFilesDir));
+		
+		if (localQueryFile != null) {
+			List<String> queryPhrases = new ArrayList<String>();
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					local.open(new Path(localQueryFile))));
+			String line;
+			while((line = br.readLine()) != null) {
+				line = line.trim();
+				if (line.length() != 0)
+					queryPhrases.add(line);
+			}
+			br.close();
+			if (queryPhrases.size() != 0)
+				query  = new FilterQuery(0, null, queryPhrases.toArray(new String[queryPhrases.size()]));
+		}
+//		if (query == null) { 
+//			query = new FilterQuery();
+//			query.locations(new double[][] {{-166.00,70.38,-48.92,17.98}}); // candada and us.
+//		}
+		
+		RawStreamListener listener = new RawStreamListener() {
+			int count = 0;
+			BufferedWriter bw = null;
 			@Override
-	        public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {}
+			public void onException(Exception arg0) {
+				log.warn(arg0.getMessage());
+			}
+			
 			@Override
-	        public void onException(Exception ex) {
-	            ex.printStackTrace();
-	        }
-			@Override
-			public void onScrubGeo(long arg0, long arg1) {}
-			@Override
-			public void onStallWarning(StallWarning arg0) {}
-			@Override
-	        public void onStatus(Status status) {
-				String rawJSON = DataObjectFactory.getRawJSON(status);
-				synchronized (buffer) {
-					buffer.add(rawJSON);
+			public void onMessage(String json) {
+				try {
+					if (bw == null || count == 100 * 1024) {
+						if (bw != null)
+							bw.close();
+						bw = new BufferedWriter(new OutputStreamWriter(
+								hdfs.create(new Path(tweetsFilesDir + "/"
+										+ "tweets." + new SimpleDateFormat("yyMMddHHmmss").format(new Date()) + ".dat"))));
+						count = 0;
+					}
+					json.replaceAll("\n", " ");
+					bw.write(json + "\n");
+					count++;
+				} catch (IOException e) {
+					log.warn(e.getMessage());
+					log.warn("missed tweet: " + json);
 				}
-	        }
-			@Override
-	        public void onTrackLimitationNotice(int numberOfLimitedStatuses) {}
-	    };
-	    twitterStream = new TwitterStreamFactory().getInstance();
+			}
+		};
+		
+		TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
 	    twitterStream.addListener(listener);
-	    // sample() method internally creates a thread which manipulates TwitterStream and calls these adequate listener methods continuously.
-	    twitterStream.filter(query);
-	}
-	/**
-	 * function to read tweets
-	 * @return
-	 */
-	public List<String> read() {
-		List<String> ret = null;
-		synchronized (buffer) {
-			ret = new ArrayList<String>(buffer.size());
-			ret.addAll(buffer);
-			buffer.clear();
-		}
-		return ret;
-	}
-	
-	public void setQuery(String[] keywords) {
-		query = new FilterQuery(0, null, keywords);
-	}
-	
-	public boolean start() {
-		if (loadThread != null) {
-			return false;
-		}
-		
-		stop = false;
-		loadThread = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				load();
-			}
-		});
-		loadThread.start();
-		return true;
+	    if (query != null)
+	    	twitterStream.filter(query);
+	    else
+	    	twitterStream.sample();
 	}
 }
